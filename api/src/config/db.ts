dd06033b94d6e3ff30c28754b21d7dd062fe4643
mofareedd <m.fareeed1997@gmail.com>
2024-09-17 06:48:48 +0300

    feat: db config

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -1,8 +1,16 @@
+import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg";
 import { env } from "./env";
+import { Database } from "@/constant/types";
 
-const pool = new Pool({
-  connectionString: env.POSTGRES_HOST,
-  max: 20,
-  idleTimeoutMillis: 30000,
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    connectionString: env.POSTGRES_HOST,
+    max: 20,
+    idleTimeoutMillis: 30000,
+  }),
+});
+
+export const db = new Kysely<Database>({
+  dialect,
 });
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import { db } from "./config/db";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get("/", async (req, res) => {
+  try {
+    const users = await db.selectFrom("users").selectAll().execute();
+
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 });
 
 export { app };
